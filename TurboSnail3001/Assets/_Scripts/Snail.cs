4f9de9f5d4fbaee945a289942562d81deb20dc7f
Marcin Radomski <marcin@mradomski.pl>
2019-01-25 21:15:55 +0100

    Add Snail prefab

---
+++ TurboSnail3001/Assets/_Scripts/Snail.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Snail : MonoBehaviour
+{
+    private Vector3 Velocity {
+        get {
+            return this.gameObject.GetComponent<Rigidbody>().velocity;
+        }
+        set {
+            this.gameObject.GetComponent<Rigidbody>().velocity = value;
+        }
+    }
+
+    public float Speed {
+        get {
+            return Velocity.magnitude;
+        }
+        set {
+            Velocity = Velocity.normalized * value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    [Button]
+    void Update()
+    {
+    }
+}
