4b6523dd1d69c6abdea9e160f448f9249fa1f06b
Marcin Radomski <marcin@mradomski.pl>
2019-01-25 22:59:21 +0100

    Make Snail use WASD for movement

--- TurboSnail3001/Assets/_Scripts/Snail.cs
+++ TurboSnail3001/Assets/_Scripts/Snail.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -11,23 +12,35 @@ public class Snail : MonoBehaviour
         get { return Body.velocity; }
         set { Body.velocity = value; }
     }
+    private Transform Transform { get { return Body.transform; }}
 
-    public float Speed
+    private Vector3 Position { get { return Body.position; } }
+    private float Speed
     {
         get { return Velocity.magnitude; }
-        set { Velocity = Body.transform.forward.normalized * value; }
+        set { Velocity = Transform.forward.normalized * value; }
     }
+
+    private float MoveSpeed;
+    private float RotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Speed = 1.0f;
     }
 
     // Update is called once per frame
     [Button]
     void Update()
     {
-        Speed = 1.0f;
+        if (Input.GetKeyDown(KeyCode.S)) { MoveSpeed = -1.0f; }
+        if (Input.GetKeyDown(KeyCode.W)) { MoveSpeed = +1.0f; }
+        if (Input.GetKeyDown(KeyCode.A)) { RotationSpeed = -1.0f; }
+        if (Input.GetKeyDown(KeyCode.D)) { RotationSpeed = +1.0f; }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) { RotationSpeed = 0.0f; }
+
+        Speed = MoveSpeed;
+        Transform.Rotate(0.0f, RotationSpeed, 0.0f);
     }
 }
