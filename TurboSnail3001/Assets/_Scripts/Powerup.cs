6603ab55aa7f57a45a50c0531caec13639979bab
Marcin Radomski <marcin@mradomski.pl>
2019-01-26 00:05:39 +0100

    Add Powerup

---
+++ TurboSnail3001/Assets/_Scripts/Powerup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Snail snail = other.gameObject.GetComponentInParent<Snail>();
+        if (snail != null) {
+            snail.MoveSpeed = 10.0f;
+            Destroy(this.gameObject);
+        }
+    }
+}
