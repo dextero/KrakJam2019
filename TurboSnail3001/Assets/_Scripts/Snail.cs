9a7f17ac6f57a2b1ff6f758cac4e7b7cd06fc995
Marcin Radomski <marcin@mradomski.pl>
2019-01-25 21:25:39 +0100

    Make snail move, slowly but surely

--- TurboSnail3001/Assets/_Scripts/Snail.cs
+++ TurboSnail3001/Assets/_Scripts/Snail.cs
@@ -5,33 +5,29 @@ using UnityEngine;
 
 public class Snail : MonoBehaviour
 {
-    private Vector3 Velocity {
-        get {
-            return this.gameObject.GetComponent<Rigidbody>().velocity;
-        }
-        set {
-            this.gameObject.GetComponent<Rigidbody>().velocity = value;
-        }
+    private Rigidbody Body { get { return this.gameObject.GetComponent<Rigidbody>(); } }
+    private Vector3 Velocity
+    {
+        get { return Body.velocity; }
+        set { Body.velocity = value; }
     }
 
-    public float Speed {
-        get {
-            return Velocity.magnitude;
-        }
-        set {
-            Velocity = Velocity.normalized * value;
-        }
+    public float Speed
+    {
+        get { return Velocity.magnitude; }
+        set { Velocity = Body.transform.forward.normalized * value; }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     [Button]
     void Update()
     {
+        Speed = 1.0f;
     }
 }
