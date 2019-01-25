3be0a317012e80371016110c3e3a9dccc2f5f001
Marcin Radomski <marcin@mradomski.pl>
2019-01-26 00:06:07 +0100

    Add simple HUD

---
+++ TurboSnail3001/Assets/_Scripts/SpeedDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SpeedDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject snailObject;
+
+    private Snail Snail {
+        get { return snailObject.GetComponent<Snail>(); }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshProUGUI>().SetText(string.Format("{0:0.0} fasts/s", Snail.MoveSpeed));
+    }
+}
