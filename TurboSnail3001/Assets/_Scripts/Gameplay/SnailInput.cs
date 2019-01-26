0b5b69be2dc67a83d32c9229fdfc0b66650bbce7
Marcin Radomski <marcin@mradomski.pl>
2019-01-26 17:56:02 +0100

    Spiral track test

--- TurboSnail3001/Assets/_Scripts/Gameplay/SnailInput.cs
+++ TurboSnail3001/Assets/_Scripts/Gameplay/SnailInput.cs
@@ -50,7 +50,7 @@ namespace TurboSnail3001
             }
 
             /* calculate velocity */
-            float velocity = (left +  right);
+            float velocity = 0.5f;
 
             /* todo: for debug only */
             if (UnityEngine.Input.GetKey(KeyCode.LeftArrow))
