065ff72776ccf70bffd68ed3c9f3a3e21b1ef8d3
mcpgnz <daniel.wojnarowski@moonlit.games>
2019-01-26 03:52:57 +0100

    feat(Menu): added menu scene

---
+++ TurboSnail3001/Assets/_Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    #region Inspector Variables
+    [SerializeField, FoldoutGroup("UI")] private Button _Start;
+    [SerializeField, FoldoutGroup("UI")] private Button _Highscore;
+    [SerializeField, FoldoutGroup("UI")] private Button _Calibration;
+    [SerializeField, FoldoutGroup("UI")] private Button _Exit;
+    #endregion Inspector Variables
+
+    #region Unity Methods
+    private void Awake()
+    {
+        _Start.onClick.AddListener(OnStart);
+        _Highscore.onClick.AddListener(OnHighscore);
+        _Calibration.onClick.AddListener(OnCalibration);
+        _Exit.onClick.AddListener(OnExit);
+    }
+    #endregion Unity Methods
+
+    #region Private Methods
+    private void OnStart()
+    {
+    }
+
+    private void OnHighscore()
+    {
+    }
+
+    private void OnCalibration()
+    {
+    }
+    private void OnExit()
+    {
+        Application.Quit();
+    }
+    #endregion Private Methods
+}
