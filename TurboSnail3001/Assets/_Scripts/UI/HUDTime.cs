df909ac08c638e18aec1c56b3689ab3888c6a94a
Marcin Radomski <marcin@mradomski.pl>
2019-01-27 13:06:01 +0100

    Fix scores for losers

--- TurboSnail3001/Assets/_Scripts/UI/HUDTime.cs
+++ TurboSnail3001/Assets/_Scripts/UI/HUDTime.cs
@@ -11,16 +11,19 @@ public class HUDTime : MonoBehaviour
         GetComponent<TextMeshProUGUI>().SetText("0:00.000");
     }
 
+    public static string TimeToString(float time) {
+        int mins = (int)Mathf.Floor(time / 60.0f);
+        int secs = (int)Mathf.Floor(time % 60.0f);
+        int msecs = (int)((time % 1.0f) * 1000.0f);
+        return string.Format("{0:D01}:{1:D02}.{2:D03}", mins, secs, msecs);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (GameController.Instance.IsRunning) {
             float time = GameController.Instance.GameTime;
-            int mins = (int) Mathf.Floor(time / 60.0f);
-            int secs = (int) Mathf.Floor(time % 60.0f);
-            int msecs = (int) ((time % 1.0f) * 1000.0f);
-            string text = string.Format("{0:D01}:{1:D02}.{2:D03}", mins, secs, msecs);
-            GetComponent<TextMeshProUGUI>().SetText(text);
+            GetComponent<TextMeshProUGUI>().SetText(TimeToString(time));
         }
     }
 }
