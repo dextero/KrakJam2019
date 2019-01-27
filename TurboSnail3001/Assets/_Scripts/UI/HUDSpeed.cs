69d13f03fefa912000c53ba5b0633340b105da31
Marcin Radomski <marcin@mradomski.pl>
2019-01-27 10:00:12 +0100

    Add missing scripts

---
+++ TurboSnail3001/Assets/_Scripts/UI/HUDSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TurboSnail3001;
+using UnityEngine;
+
+public class HUDSpeed : MonoBehaviour
+{
+    [SerializeField] private Snail Snail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshProUGUI>().SetText(string.Format("{0:0.0} fasts/h", Snail.Speed));
+    }
+}
---
+++ TurboSnail3001/Assets/_Scripts/UI/HUDTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUDTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().SetText("0:00.000");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.Instance.IsRunning) {
+            float time = GameController.Instance.GameTime;
+            int mins = (int) Mathf.Floor(time / 60.0f);
+            int secs = (int) Mathf.Floor(time % 60.0f);
+            int msecs = (int) ((time % 1.0f) * 1000.0f);
+            string text = string.Format("{0:D01}:{1:D02}.{2:D03}", mins, secs, msecs);
+            GetComponent<TextMeshProUGUI>().SetText(text);
+        }
+    }
+}
